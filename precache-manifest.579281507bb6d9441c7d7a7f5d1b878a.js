23db028e4d60c5e64def6398660a3312dd6e162b
olegdokuchaev <badguling@gmail.com>
2020-01-06 22:17:14 +0300

    Updates

--- precache-manifest.579281507bb6d9441c7d7a7f5d1b878a.js
+++ precache-manifest.579281507bb6d9441c7d7a7f5d1b878a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "753dc14b5a371c0b4ca250d077041066",
+    "revision": "08c1d8bbb8ca10aa9a614c7f803ef604",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.2cfa1b23.chunk.css"
   },
   {
-    "revision": "39ecdf545a3f2a3403fe",
+    "revision": "a2fe9ce34267bf3a965e",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.6b547687.chunk.js.LICENSE"
   },
   {
-    "revision": "39ecdf545a3f2a3403fe",
-    "url": "./static/js/main.f46e7bac.chunk.js"
+    "revision": "a2fe9ce34267bf3a965e",
+    "url": "./static/js/main.3cc7d1a6.chunk.js"
   },
   {
     "revision": "5ca38037eefa827c4278",
