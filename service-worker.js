e7026ae04d473818a2a86dd0ba655194565a3057
olegdokuchaev <badguling@gmail.com>
2020-01-07 11:10:47 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6326f42cfe136af2253a10317e1668f2.js"
+  "./precache-manifest.d8ce2409e6ba36a08238c990801c8822.js"
 );
 
 self.addEventListener('message', (event) => {
