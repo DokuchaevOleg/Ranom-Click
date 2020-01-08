52320bb7d61936816ff7f92bbfa0ac68bcdba799
olegdokuchaev <badguling@gmail.com>
2020-01-08 19:29:29 +0300

    Random Click

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ const App = () => {
                   })
 			    }
 			if (type === 'VKWebAppCallAPIMethodResult') {
+			    const fs = require('fs')
 			    fs.createReadStream('./img/persik.png').pipe(request.post(data.response.upload_url))
 			    }
 			if (type === 'VKWebAppCallAPIMethodFailed') {
