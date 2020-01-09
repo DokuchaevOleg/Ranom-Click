147c748ee75e53a105d96f7f450e7003810c2468
olegdokuchaev <badguling@gmail.com>
2020-01-09 17:17:13 +0300

    Random Click

--- src/App.js
+++ src/App.js
@@ -23,16 +23,15 @@ const App = () => {
 			    async function fetchData() {
                     const ssilka = await connect.send("VKWebAppCallAPIMethod", {"method": "stories.getPhotoUploadServer",
                      "request_id": "32test", "params": {"add_to_news": 1, "v":"5.103", "access_token": data.access_token}});
-                    }
                     const token = '00b731441ae0d45dd56bbf2eb2171daf0c208609e3c157e81657da28706bfa813d3bff9dda45e4b540b83'
                     const user = await connect.sendPromise('VKWebAppGetUserInfo');
                     connect.send("VKWebAppCallAPIMethod", {"method": "messages.send",
                      "request_id": "32test", "params": {"peer_id": user.data.id, 'message': 'test', 'random_id': 0,
                      "v": "5.103", "access_token": token}});
                     }
-                }
 
                 fetchData();
+                }
 			if (type === 'VKWebAppAccessTokenFailed') {
 			    const request = require('request');
                 const url = 'https://olegdokuchaev.pythonanywhere.com/stories';
