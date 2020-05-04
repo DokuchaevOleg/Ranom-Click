a7af94a3f4bad24ce2eb53731a6e116f70256118
olegdokuchaev <badguling@gmail.com>
2020-05-04 11:38:18 +0300

    Random Click

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -7,19 +7,20 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
-import random_click from '../img/Random_Click.png';
+import random_click from '../img/Random_Click-3.png';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Random Click</PanelHeader>
+		<PanelHeader>Акция Random Click!</PanelHeader>
 		<img className="Persik" src={random_click} alt="Persik The Cat"/>
 		{fetchedUser &&
-		<Group title="Бесплатные Клики!">
+		<Group >
+		    <p className='text3'><b><font color="#5b81b4">Бесплатные Клики!</font></b></p>
 		    <p className='text'>
-			<b>{fetchedUser.first_name}</b>, нажми <b>ОПУБЛИКОВАТЬ</b> и получи до <b>555 Кликов</b>!
+			<b><font color="#5b81b4">{fetchedUser.first_name}</font></b>, нажми <font color="#5b81b4"><b>ОПУБЛИКОВАТЬ</b></font> и получи до <font color="#5b81b4"><b>555 Кликов</b>!</font>
 			</p>
 			<p className='text'>
-			Клики можно получать <b>каждые 24 часа</b>!
+			Клики можно получать <font color="#5b81b4"><b>каждые 24 часа</b>!</font>
 			</p>
 			<p className='text2'>
 			Поторопись, скоро акция исчезнет!
